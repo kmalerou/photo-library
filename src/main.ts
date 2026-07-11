ac1de4cf75b7dc66629080248096be8b40264ee0
Kelly Malerou <kmalerou@gmail.com>
2026-07-11 14:20:33 +0300

    refactor: extract bootstrap providers into app.config.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
-import { provideZoneChangeDetection } from "@angular/core";
-import { bootstrapApplication } from "@angular/platform-browser";
-import { provideRouter } from "@angular/router";
-import { routes } from "./app/app.routes";
-import { App } from "./app/app";
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
 
-
-bootstrapApplication(App, {
-    providers: [provideZoneChangeDetection(), provideRouter(routes)]
-})
-  .catch(err => console.error(err));
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));

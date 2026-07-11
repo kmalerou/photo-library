4fa22b5c9ac4f7bd563ad3f23d51e708d2683726
Kelly Malerou <kmalerou@gmail.com>
2026-07-11 18:49:10 +0300

    feat: scaffold photo-stream, favorites, and photo-detail route components

---
+++ src/app/features/photo-detail/photo-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-detail',
+  imports: [],
+  templateUrl: './photo-detail.html',
+  styleUrl: './photo-detail.scss',
+})
+export class PhotoDetail {}
